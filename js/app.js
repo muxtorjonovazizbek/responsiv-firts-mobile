883172b48d89937d0f66c85fee8dccf2e91c3d25
muxtorjonovazizbek <azizbekmukhtorjonov581@gmail.com>
2024-06-10 14:52:31 +0500

    loader

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let header = document.querySelector(".header");
 let toggleBtn = document.querySelector(".nav__bar__btn");
 let headerList = document.querySelector(".nav__link__items");
+const loader = document.querySelector(".loader");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 10) {
@@ -13,3 +14,12 @@ window.addEventListener("scroll", () => {
 toggleBtn.addEventListener("click",()=>{
   headerList.classList.toggle("nav__list-show");
 })
+
+window.addEventListener("load", () =>  {
+  
+  loader.classList.add("loader--hidden");
+
+  document.querySelector(".loader").addEventListener("transitionend", () => {
+      document.body.removeChild(document.querySelector(".loader"));
+  });
+});
